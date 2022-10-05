631d475e5820cf2a5fe6b8bf1cddc9002131189a
MonikaRaghav <105781189+MonikaRaghav@users.noreply.github.com>
2022-10-05 18:41:21 +0530

    Update login.js
    changes made in login page

---
+++ apis/login.js
@@ -0,0 +1,15 @@
+const express= require("express");
+
+var router = express.Router();
+app.use(router);
+
+
+router.post("/signup",function(req,res){
+
+});
+
+router.get("/login",function(req,res){
+
+});
+
+module.exports = router;
