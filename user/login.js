b87a7ca1b919aac49be975b565f6694c7ac885e9
Leksyking <gbemilekeogundipe@gmail.com>
2022-10-12 21:54:27 +0100

    add login functionality

--- user/login.js
+++ user/login.js
@@ -3,17 +3,16 @@ const app = express();
 var router = express.Router();
 app.use(router);
 
-
-
 //mysql connector pool
 const pool=require("./mysqlconnector");
 
-router.post("/signup",function(req,res){
-
-});
-
-router.get("/login",function(req,res){
-
+router.post("/login", function(req, res){
+    const { email } = req.body;
+    
+    pool.query("SELECT * FROM users WHERE email = ?", email, function (error, results, fields){
+        if (error) throw error;
+        res.status(201).json({error: false, results, msg: "You have successfully logged in"});
+    });
 });
 
 module.exports = router;
