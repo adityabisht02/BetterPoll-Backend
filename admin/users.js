d0d0f461cbe7bd55e0a9644a109c90f394f54621
Aditya <adityabisht02@gmail.com>
2022-10-13 20:11:59 +0530

    added admin getuserdetails and get singleuserdetauls

--- admin/users.js
+++ admin/users.js
@@ -3,13 +3,8 @@ const pool = require("../mysqlconnector");
 const router = express.Router();
 
 
-const addNewUser = (req, res) => {
-    
-}
-
 router.post("/add-new-user", (req,res)=>{
     const {name, email, phone} = req.body;
-    console.log(req.body);
     let sql = `INSERT INTO users (name, email, phone) VALUES (?, ?, ?);`;
     pool.query(sql, [name, email, phone], function (error, results, fields) {
         if (error) throw error;
@@ -17,8 +12,25 @@ router.post("/add-new-user", (req,res)=>{
     });
 });
 
-router.post("/get-user", function (req, res) {
-    console.log(req.body)
-    res.send(req.body)
+router.get("/get-user-details", function (req, res) {
+    const username = req.body;
+    if(!username){
+        return res.status(400).json({success: false,msg:"Enter a valid user!"});
+    }
+    let sql='SELECT * FROM users WHERE name = ?';
+
+    pool.query(sql,username,function (error, results, fields) {
+        if (error) throw error;
+        res.status(201).json({success: true, results, msg: "User retrieved successfully"});
+    });
+})
+
+router.get("/get-all-user-details",function (req, res){
+    let sql='SELECT * FROM users';
+    pool.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        res.status(201).json({success: true, results, msg: "Users information retrieved successfully"});
+    });
+
 })
 module.exports = router;
