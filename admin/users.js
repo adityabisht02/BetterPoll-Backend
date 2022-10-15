7371909676dc99819acb8c38bcc36a0a21766aff
Harsh_Garg <garg.harsh0014@gmail.com>
2022-10-15 12:19:43 +0530

    added an api which will allow the admin to adit any users details and also changed the datatype of phone field of users table from int to varchar

--- admin/users.js
+++ admin/users.js
@@ -33,4 +33,63 @@ router.get("/get-all-user-details",function (req, res){
     });
 
 })
+
+router.put('/edit-user-details', async (req, res) => {
+    const requestPayload = {
+        name : req.body.name,
+        email : req.body.email,
+        phone : req.body.phone
+    };
+
+    // if username is not present then how can we identify that which user's data
+    // should be changed
+    if(!requestPayload.name) {
+        return res.status(400).json({
+            message : "Username is missing!"
+        });
+    }
+
+    // if email and phone both not present 
+    if(!requestPayload.email && !requestPayload.phone) {
+        return res.status(400).json({
+            message : "Incomplete/Invalid request payload!"
+        })
+    }
+    
+    pool.query(`SELECT COUNT(*) as count FROM users WHERE name = '${requestPayload.name}';`, (error, result) => {
+        if(error) {
+            return res.status(500).json({
+                message : "internal server error"
+            });
+        }
+
+        if(result[0].count < 1) {
+            return res.status(400).json({
+                message : "Invalid username!"
+            });
+        }
+
+        let query = ``;
+
+        if(requestPayload.email && requestPayload.phone) {
+            query = `UPDATE users SET email = '${requestPayload.email}', phone = ${requestPayload.phone} WHERE name = '${requestPayload.name}';`;
+        } else if(requestPayload.email) {
+            query = `UPDATE user SET email = '${requestPayload.email}' WHERE name = '${requestPayload.name}';`;
+        } else if(requestPayload.phone) {
+            query = `UPDATE user SET phone = ${requestPayload.phone} WHERE name = '${requestPayload.name}';`;
+        } 
+
+        pool.query(query, (error, result) => {
+            if(error) {
+                return res.status(500).json({
+                    message : "internal server error"
+                });
+            } 
+            return res.status(200).json({
+                message : "user updated"
+            });
+        })
+    });
+})
+
 module.exports = router;
