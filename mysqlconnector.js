fe38798e421d39c33af51c2b358e266d75c6e327
Harpreet <harkhush1102@gmail.com>
2022-10-27 00:45:37 +0530

    Refactored the db connection with some necessary requirements

--- mysqlconnector.js
+++ mysqlconnector.js
@@ -1,14 +1,14 @@
 //mysql driver
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-    connectionLimit : 10, //important
-    host     : 'localhost',
-    user     : 'root',
-    password : process.env.SQL_PASSWORD,
-    database : 'betterpoll'
+  connectionLimit: 10, //important
+  host: "localhost",
+  user: "root",
+  password: process.env.SQL_PASSWORD,
+  database: "betterpoll",
+  port: "3306",
 });
 
-
-
-module.exports=pool;
+module.exports = pool;
