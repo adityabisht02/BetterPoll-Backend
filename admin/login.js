4034651fa44fcc16fbfe98f276862547c78d51b1
Harpreet <harkhush1102@gmail.com>
2022-10-27 14:05:22 +0530

    [Feat]: Created the Admin Sign-up API with features of storing the hashed password in admin table

--- admin/login.js
+++ admin/login.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const app=express();
+const app = express();
 const pool = require("../mysqlconnector");
 const router = express.Router();
 
+const bcrypt = require("bcrypt");
 
 //sign-up api for admins
-
+router.post("/sign-up", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { name, password, email } = req.body;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    let sql = `INSERT INTO admin (username, password, email) VALUES (?, ?, ?);`;
+    pool.query(sql, [name, hashedPassword, email], function (error, results) {
+      if (error) throw error;
+      res
+        .status(201)
+        .json({ success: true, results, msg: "Admin created successfully" });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //login api for admins
 
-
-module.exports=router;
+module.exports = router;
